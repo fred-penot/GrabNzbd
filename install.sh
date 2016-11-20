1691371db3bff0c90ed30129a8de45bea8e6951e
fred-penot <frederic.penot@gmail.com>
2016-11-20 01:00:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,21 +39,21 @@ echo "## Installation des plugins bower ##" >> ${path_install}/install.log
 echo "####################################" >> ${path_install}/install.log
 
 cd ${path_install}/App
-bower install >> ${path_install}/install.log
+bower install --allow-root >> ${path_install}/install.log
 
 echo "###########################" >> ${path_install}/install.log
 echo "## Installation du hooks ##" >> ${path_install}/install.log
 echo "###########################" >> ${path_install}/install.log
 
-cd ${path_install}/App
-ionic hooks add >> ${path_install}/install.log
+#cd ${path_install}/App
+#ionic hooks add >> ${path_install}/install.log
 
 echo "###########################################" >> ${path_install}/install.log
 echo "## Installation de la plateforme Android ##" >> ${path_install}/install.log
 echo "###########################################" >> ${path_install}/install.log
 
-cd ${path_install}/App
-ionic platform add android >> ${path_install}/install.log
+#cd ${path_install}/App
+#ionic platform add android >> ${path_install}/install.log
 
 echo "#############################" >> ${path_install}/install.log
 echo "## Modification des droits ##" >> ${path_install}/install.log
